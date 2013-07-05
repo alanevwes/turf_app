4497a09746f6d05e663f6d3d1ea476f9a8498656
alanevwes <alanevwes@gmail.com>
2013-07-05 11:39:15 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurfApp::Application.config.secret_token = 'c75ff97e6a8ab7e4552cc76d18d3d21ea5f7be062caf7575a544b90abb474ffb8f3549ecb0fd748cdd1b715002c8a73264203a172ea99b0c760c7eae460b6371'
